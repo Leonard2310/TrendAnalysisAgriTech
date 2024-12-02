78efa24c233f2e675aeaf5cfcbbfea349f85fb4f
Leonardo Catello <leonardo.catello23@gmail.com>
2024-12-02 16:46:04 +0100

    update semplification to code

--- code/WebApp/graphs.py
+++ code/WebApp/graphs.py
@@ -48,7 +48,7 @@ def train_graphs(folder_path):
             fig.add_trace(go.Scatter(x=results.index, y=results['Forecast_Test'], mode='lines', name='Forecast (Test)', line=dict(color='green')))
 
             # Aggiunta degli intervalli di confidenza
-            if all(keyword not in csv_file for keyword in ['DecisionTree', 'GradientBoosting', 'RandomForest']):
+            if 'ARIMAX' in csv_file:
                 fig.add_trace(go.Scatter(
                     x=results.index.tolist() + results.index[::-1].tolist(),
                     y=results['Upper_CI'].tolist() + results['Lower_CI'][::-1].tolist(),
